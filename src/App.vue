5f2846684c8bc5add44ec34e48cd96789f94a7af
Jonas Karl <59976673+jonas080301@users.noreply.github.com>
2022-01-25 11:07:11 +0100

    Initial upload

---
+++ src/App.vue
@@ -0,0 +1,173 @@
+
+
+<template>
+
+     
+  <header class="text-gray-100 bg-gray-900 body-font shadow w-full">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row justify-between items-center">
+          <a
+              class="flex order-first title-font font-medium items-center mb-4 md:mb-0">
+              <svg style="height: 30px; margin-top: 10px; margin-bottom: 10px;" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 119.45 122.88"><title>email-address</title><path fill="#fff" stroke="#fff" d="M0,58.68V53.56L39.82,88.38.52,119.18A6.14,6.14,0,0,1,0,116.71v-58Zm82-6a24,24,0,0,1-1,6.19,15.32,15.32,0,0,1-2.48,4.85,10.89,10.89,0,0,1-3.83,3.18A11.09,11.09,0,0,1,69.83,68a8.69,8.69,0,0,1-2.13-.25A7.6,7.6,0,0,1,65.79,67a6.7,6.7,0,0,1-1.58-1.21,6.8,6.8,0,0,1-.69-.82,8.54,8.54,0,0,1-6.69,2.92,8,8,0,0,1-2.52-.39,7.53,7.53,0,0,1-2.19-1.19l0,0a7.89,7.89,0,0,1-1.71-1.88,10.11,10.11,0,0,1-1.19-2.52v0a15.7,15.7,0,0,1-.55-6.27v0a23.36,23.36,0,0,1,1.46-5.95,16.59,16.59,0,0,1,2.83-4.71,12.06,12.06,0,0,1,4-3.09,11.11,11.11,0,0,1,4.79-1,14.3,14.3,0,0,1,3.67.45,11.15,11.15,0,0,1,3.17,1.39l1.63,1.09a.65.65,0,0,1,.28.59L69.11,59.36a3.29,3.29,0,0,0,.29,2.22,1.75,1.75,0,0,0,1.54.61,2.63,2.63,0,0,0,1.61-.56A5.63,5.63,0,0,0,74,59.74a12,12,0,0,0,1.09-3,20.78,20.78,0,0,0,.47-3.83,24.13,24.13,0,0,0-.65-7.4,13.59,13.59,0,0,0-2.76-5.39,11.76,11.76,0,0,0-4.67-3.32A17.51,17.51,0,0,0,61,35.68a16.06,16.06,0,0,0-4.45.6,14.57,14.57,0,0,0-3.91,1.79A15.31,15.31,0,0,0,49.32,41a18.56,18.56,0,0,0-2.54,3.93,24,24,0,0,0-1.69,4.76,29.77,29.77,0,0,0-.74,5.42h0A25.78,25.78,0,0,0,45,62.81a14,14,0,0,0,2.77,5.57,11.75,11.75,0,0,0,4.76,3.38,18.42,18.42,0,0,0,6.77,1.15,19.82,19.82,0,0,0,2.16-.12c.74-.08,1.51-.21,2.28-.37s1.48-.33,2.12-.52a15.05,15.05,0,0,0,1.7-.61.64.64,0,0,1,.84.34.55.55,0,0,1,0,.17L69.41,76a.67.67,0,0,1-.28.68,10.53,10.53,0,0,1-2,1,17.2,17.2,0,0,1-2.47.76,23.7,23.7,0,0,1-2.73.49,23.18,23.18,0,0,1-2.75.16,24.74,24.74,0,0,1-12.17-2.77,16.89,16.89,0,0,1-4.39-3.45,18.16,18.16,0,0,1-3.09-4.79l0,0a24,24,0,0,1-1.73-5.95A35.46,35.46,0,0,1,37.41,55a32.6,32.6,0,0,1,1-6.86,28,28,0,0,1,2.36-6.12A23.13,23.13,0,0,1,44.37,37a21.6,21.6,0,0,1,4.74-3.82,22.27,22.27,0,0,1,5.65-2.37A24.52,24.52,0,0,1,61.07,30a22.8,22.8,0,0,1,11.5,2.77,17.53,17.53,0,0,1,4.28,3.4,18.32,18.32,0,0,1,3,4.59,22.32,22.32,0,0,1,1.76,5.6A28.62,28.62,0,0,1,82,52.71Zm-25.7,3a15.3,15.3,0,0,0,0,2.79,5.85,5.85,0,0,0,.48,1.9,2.42,2.42,0,0,0,.83,1,2.13,2.13,0,0,0,1.19.32,2.1,2.1,0,0,0,.89-.2,3.08,3.08,0,0,0,.87-.67,5.64,5.64,0,0,0,.83-1.17,10.13,10.13,0,0,0,.69-1.58l1-11-.17,0a5.62,5.62,0,0,0-.74,0,5,5,0,0,0-2.25.49A4.51,4.51,0,0,0,58.27,49a9.45,9.45,0,0,0-1.2,2.7,23.81,23.81,0,0,0-.73,4ZM.15,43.47C1,40.14,5.72,38,8.34,36.14V21.85a6,6,0,0,1,6-6H36.81L58.26.57A3.24,3.24,0,0,1,62,.59L83.08,15.85h22a6,6,0,0,1,6,6V36.1c2.82,2,8.34,4.72,8.34,8.49v.15L105.11,56.83v-35H14.34v34L.15,43.47Zm119.3,11.31v61.93a6.12,6.12,0,0,1-1.42,3.93L78.34,89.43l41.11-34.65Zm-11,68.1H8.26L45.71,93.53l11.15,9.75a3.83,3.83,0,0,0,5.15-.09L72.33,94.5l36.09,28.38Z"/></svg>
+              <span class="ml-3 text-xl">mailto Generator</span>
+          </a>
+          <div class="inline-flex justify-end ml-5 ">
+              <a href="https://www.github.com/jonas080301" class="bg-gray-600 hover:bg-gray-500 text-white ml-4 py-2 px-3 rounded-lg">
+                  Github
+              </a>
+              <a href="https://it-karl.de" class="bg-indigo-700 hover:bg-indigo-500 text-white ml-4 py-2 px-3 rounded-lg">
+                  About
+              </a>
+          </div>
+
+      </div>
+  </header>
+  <div class="flex w-full flex-col justify-items-center items-center">
+    <div class="bg-white shadow rounded-lg p-6 w-full max-w-7xl m-5">
+      <div class="grid lg:grid-cols-2 gap-6">
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 lg:col-span-2">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="to" class="bg-white text-gray-600 px-1">To (Empfänger)</label>
+            </p>
+          </div>
+          <p>
+            <input @keyup="format()" v-model="to" placeholder="example@mail.com" id="to" autocomplete="true" tabindex="0" type="email" class="py-1 px-1 text-gray-900 outline-none block h-full w-full">
+          </p>
+        </div>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="cc" class="bg-white text-gray-600 px-1">CC</label>
+            </p>
+          </div>
+          <p>
+            <input @keyup="format()" v-model="cc" placeholder="example@mail.com" id="cc" autocomplete="false" tabindex="0" type="mail" class="py-1 px-1 outline-none block h-full w-full">
+          </p>
+        </div>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="bcc" class="bg-white text-gray-600 px-1">BCC</label>
+            </p>
+          </div>
+          <p>
+            <input @keyup="format()" v-model="bcc" placeholder="example@mail.com" id="bcc" autocomplete="false" tabindex="0" type="mail" class="py-1 px-1 outline-none block h-full w-full">
+          </p>
+        </div>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 lg:col-span-2">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="subject" class="bg-white text-gray-600 px-1">Subject (Betreff)</label>
+            </p>
+          </div>
+          <p>
+            <input @keyup="format()" v-model="subject" placeholder="Your enquiry..." id="subject" autocomplete="false" tabindex="0" type="text" class="py-1 px-1 outline-none block h-full w-full">
+          </p>
+        </div>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 lg:col-span-2">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="body" class="bg-white text-gray-600 px-1">Body (Mail Text)</label>
+            </p>
+          </div>
+          <p>
+            <textarea @keyup="format()" v-model="body" placeholder="Dear Max Smith, ..." id="body" autocomplete="false" tabindex="0" type="textarea" class="py-1 px-1 outline-none block h-full w-full"></textarea >
+          </p>
+        </div>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 lg:col-span-2">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="linktext" class="bg-white text-gray-600 px-1">Link Text</label>
+            </p>
+          </div>
+          <p>
+            <input @keyup="format()" v-model="linktext" placeholder="Click here" id="linktext" autocomplete="false" tabindex="0" type="mail" class="py-1 px-1 outline-none block h-full w-full">
+          </p>
+        </div>
+      </div>
+
+      <div class="border-t mt-6 pt-3">
+        <p class="text-red-700 -mt-4 mb-5 " v-for="error in errors" :key="error">{{error}}</p>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 lg:col-span-2 mt-3">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="mailto" class="bg-white text-gray-600 px-1">Output</label>
+            </p>
+          </div>
+          <p class="break-words">
+            {{formated}}
+          </p>
+
+        </div>
+        <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 lg:col-span-2 mt-6">
+          <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <p>
+              <label for="html" class="bg-white text-gray-600 px-1">HTML Element</label>
+            </p>
+          </div>
+          <p class="break-words">
+            {{"<a href='"+formated+"'>"+linktext+"</a>"}}
+          </p>
+
+        </div>
+        <a
+        :href="formated"
+        class="
+          mt-5
+          py-4
+          px-6
+          sm:px-10
+          lg:px-8
+          xl:px-10
+          inline-flex
+          items-center
+          justify-center
+          text-center text-white text-base
+          bg-blue-500
+          hover:bg-opacity-90
+          font-normal
+          rounded-lg
+        "
+      >
+        Click here to test
+      </a>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+
+
+export default {
+  name: "App",
+  data() {
+    return { 
+      
+      to: "",
+      cc: "",
+      bcc: "",
+      subject: "",
+      body: "",
+      linktext: "Click here",
+      formated: "mailto:",
+      errors: [""]
+    }
+  },
+  methods: {
+    format(){
+      const REGEX= new RegExp('^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$', 'i');
+      this.errors = [""]
+      this.to && (REGEX.test(this.to) ? "" : this.errors.push("'To' mailadress might be invalid!"));
+      this.cc && (REGEX.test(this.cc) ? "" : this.errors.push("'CC' mailadress might be invalid!"));
+      this.bcc && (REGEX.test(this.bcc) ? "" : this.errors.push("'BCC' mailadress might be invalid!"));
+      this.formated = `mailto:${this.to}${(this.cc || this.bcc || this.subject || this.body) ? "?" : ""}${this.cc ? "cc="+encodeURIComponent(this.cc) : this.cc && "cc="+encodeURIComponent(this.cc)}${this.cc && this.bcc ? "&bcc="+encodeURIComponent(this.bcc) : this.bcc && "bcc="+encodeURIComponent(this.bcc)}${(this.cc || this.bcc) && this.subject ? "&subject="+encodeURIComponent(this.subject) : this.subject && "subject="+encodeURIComponent(this.subject)}${(this.cc || this.bcc || this.subject) && this.body ? "&body="+encodeURIComponent(this.body) : this.body && "body="+encodeURIComponent(this.body)}`
+      return this.formated;
+    }
+  }
+
+}
+</script>
+<style>
+
+</style>
